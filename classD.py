ee3ce5fbe6553a320e9b06513be83da4598ab518
afsana1210 <afsanaansari1210@gmail.com>
2019-04-30 22:02:44 +0530

    class

---
+++ classD.py
@@ -0,0 +1,17 @@
+class Dog():
+
+ def _init_(self,Dbreed,name,spots):
+   
+   #ATTRIBUTES
+   #WE TAKE IN THE ARGUMENTS
+   #AND ASSIGN IT WITH SELF.ATTRIBUTES_NAME
+  self.breed=Dbreed
+  self.name=name
+  
+  #EXPECT BOOLEAN TRUE/FALSE
+  self.spots=spots
+
+my_dog=Dog(Dbreed='lab',name='tom',spots='False')
+my_dog=Dog()
+#my_dog.Dbreed
+
--- tic_toc_toe.py
+++ tic_toc_toe.py
@@ -1,6 +1,6 @@
 #from IPython.display import clear_output
-
-def display(board):
+import random
+def display_board(board):
  
  print board[7]+'|'+board[8]+'|'+board[9]
  print board[6]+'|'+board[5]+'|'+board[4]
@@ -8,9 +8,9 @@ def display(board):
 
 
 
-test_board=['#','x','o','x','o','x','o','x','o','x']
+#test_board=['#','x','o','x','o','x','o','x','o','x']
 #test_board=[' ']*10
-display(test_board)
+#display(test_board)
 
 def player_input():
   
@@ -24,17 +24,17 @@ def player_input():
   else:
     return('o','x')  
 
-player_input()
+#player_input()
 
 def place_marker(board,marker,position):
   board[position]=marker
 
 
-place_marker(test_board,'$',8)  
+#place_marker(test_board,'$',8)  
   
 
 
-def chk_won(board,marker):
+def win_chk(board,marker):
   return ((board[7]==marker and board[8]==marker and board[9]==marker) or
           (board[6]==marker and board[5]==marker and board[4]==marker) or
           (board[3]==marker and board[2]==marker and board[1]==marker) or 
@@ -44,5 +44,95 @@ def chk_won(board,marker):
           (board[9]==marker and board[5]==marker and board[3]==marker) or
           (board[7]==marker and board[5]==marker and board[1]==marker))
 
-chk_won(test_board,'x')          
-              
+#chk_won(test_board,'x')          
+
+def choose_first():
+   flip=random.randint(0,1)
+
+   if flip==0:
+     return 'player1'
+   else:
+     return 'player2'
+
+
+def space_chk(board,position):
+  return board[position] ==' '
+
+
+def player_choice(board):
+  position=0
+
+  while position not in [1,2,3,4,5,6,7,8,9] or not space_chk(board,position):
+    position=input('choose a position (1-9) :')
+    return position
+
+
+def full_board_chk(board):
+    for i in range(1,10):
+      if space_chk(board,i):
+        return False
+
+    return True   
+
+def replay():
+  choice=input('play again? enter yes or no')
+  return choice=='yes'              
+
+
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    # Reset the board
+    theBoard = [' '] * 10
+    player1_marker, player2_marker = player_input()
+    turn = choose_first()
+    print(turn + ' will go first.')
+    
+    play_game = input('Are you ready to play? Enter Yes or No.')
+    
+    if play_game.lower()[0] == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    while game_on:
+        if turn == 'Player 1':
+            # Player1's turn.
+            
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player1_marker, position)
+
+            if win_chk(theBoard, player1_marker):
+                display_board(theBoard)
+                print('Congratulations! You have won the game!')
+                game_on = False
+            else:
+                if full_board_chk(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Player 2'
+
+        else:
+            # Player2's turn.
+            
+            display_board(theBoard)
+            position = player_choice(theBoard)
+            place_marker(theBoard, player2_marker, position)
+
+            if win_chk(theBoard, player2_marker):
+                display_board(theBoard)
+                print('Player 2 has won!')
+                game_on = False
+            else:
+                if full_board_chk(theBoard):
+                    display_board(theBoard)
+                    print('The game is a draw!')
+                    break
+                else:
+                    turn = 'Player 1'
+
+    if not replay():
+        break
